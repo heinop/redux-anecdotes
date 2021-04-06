da07e74ac9d0b2613256a28c825f45a42bb21349
heinop <pekka.heino@unikie.com>
2021-04-06 12:58:42 +0300

    Task 6.13: Added service to get anecdotes from server (json-server)

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,11 +1,11 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+// const anecdotesAtStart = [
+//   'If it hurts, do it more often',
+//   'Adding manpower to a late software project makes it later!',
+//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+//   'Premature optimization is the root of all evil.',
+//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+// ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -17,7 +17,7 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+// const initialState = anecdotesAtStart.map(asObject)
 
 const compareVotes = (a1, a2) => a2.votes - a1.votes
 
@@ -37,7 +37,14 @@ export const createAnecdote = (content) => {
   }
 }
 
-const reducer = (state = initialState, action) => {
+export const initializeAnecdotes = (data) => {
+  return {
+    type: 'INITIALIZE',
+    data
+  }
+}
+
+const reducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
@@ -49,6 +56,8 @@ const reducer = (state = initialState, action) => {
       return state.map(a => a.id === id ? changedAnecdote : a).sort(compareVotes)
     case 'NEW_ANECDOTE':
       return [ ...state, action.data ].sort(compareVotes)
+    case 'INITIALIZE':
+      return action.data
     default:
       return state.sort(compareVotes)
   }
